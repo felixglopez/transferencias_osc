95e23f59f03212c1ae38291e4e384ffd453c865f
Felix G. Lopez <felixglopez@gmail.com>
2024-09-26 17:55:34 -0300

    criando faceta com maiores transferencias

--- code/transferencias_osc.R
+++ code/transferencias_osc.R
@@ -1,4 +1,4 @@
-Via linguagem de programação R
+###
 # instalação do pacote RPostgres
 library("RPostgres")
 library(DBI)
@@ -6,8 +6,8 @@ library(DBI)
 con <- dbConnect(RPostgres::Postgres(),dbname = 'portal_osc2', 
                  host = 'psql12', 
                  port = 5432,
-                 user = '',
-                 password = 'd')
+                 user = 'r1705296',
+                 password = '')
 #nota: incluir o comando bigint = ‘integer’ (con <- dbConnect(RPostgres::Postgres(),dbname = 'siape', bigint = 'integer',) evita o formato integer64 que o ggplot nao reconhece.
 
 # realizar consulta
@@ -65,3 +65,76 @@ g_transferencias <- ggplot(data = transferencia, aes(x = ano, y = total_bilhoes)
         )
 #salvando o grafico com todas as transferencias federais, por ano
 ggsave("figures/g_transferencias.png", plot = g_transferencias, width = 8, height = 6, dpi = 300)
+
+
+
+#######################################
+##AGORA OS DEZ MAIORES VALORES, POR ANO
+#######################################
+
+
+# realizar consulta
+res <- dbGetQuery(con, "WITH summed_values AS (
+    SELECT nr_orcamento_ano, 
+           nr_orcamento_cnpj, 
+           SUM(nr_vl_empenhado_def_inpc_junho_24) AS total_empenhado
+    FROM public.tb_orcamento_def_v3
+    GROUP BY nr_orcamento_ano, nr_orcamento_cnpj
+),
+ranked_values AS (
+    SELECT nr_orcamento_ano, 
+           nr_orcamento_cnpj, 
+           total_empenhado,
+           ROW_NUMBER() OVER (PARTITION BY nr_orcamento_ano ORDER BY total_empenhado DESC) AS rn
+    FROM summed_values
+)
+SELECT nr_orcamento_ano, 
+       nr_orcamento_cnpj, 
+       total_empenhado
+FROM ranked_values
+WHERE rn <= 10
+ORDER BY nr_orcamento_ano, total_empenhado DESC;")
+
+# finalizar conexão com o servidor (banco de dados)
+dbDisconnect(con)
+
+# para salvar o resultado da consulta em formato .csv
+# a função é write.csv2(tabela, "nomedatabela.csv", fileEncoding = "UTF-8").  # Lembrar de definir a pasta de trabalho (ctrl+shift+h) antes. É nela que a   # tabela será salva.A função salva automaticamente com o ";" como separador.  # Exemplo:
+write.csv2(res,"data/top_transferencia_osc.csv", fileEncoding = "UTF-8")
+
+top_trans <-  res 
+
+#renomeando a variavel
+top_trans <-  top_trans %>%
+        rename(ano = nr_orcamento_ano,
+               CNPJ = nr_orcamento_cnpj)
+
+
+top_trans  <- top_trans %>% 
+        mutate(total_empenhado_milhoes = round(total_empenhado/1000000))
+
+               
+
+#grafico para reportar os maiores valores por ano
+# Converter CNPJ para character
+top_trans$CNPJ <- as.character(top_trans$CNPJ)
+
+top_trans_filtered <- top_trans %>%
+        filter(ano != c(2001, 2021)) %>% 
+        group_by(ano) %>%
+        top_n(5, wt = total_empenhado_milhoes)
+
+
+ggplot(top_trans_filtered, aes(x = CNPJ, y = total_empenhado_milhoes)) +
+        geom_bar(stat = "identity", fill = "steelblue") +
+        facet_wrap(~ ano) +
+        labs(
+                title = "Total Empenhado (Milhões) por CNPJ por Ano",
+                x = "CNPJ",
+                y = "Total Empenhado (Milhões)"
+        ) +
+        theme_bw() +
+        theme(
+                axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                strip.text = element_text(size = 12, face = "bold")
+        )
